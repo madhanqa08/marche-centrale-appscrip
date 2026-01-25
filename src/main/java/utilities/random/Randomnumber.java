9f47839438b4e4dd7bd094e0f94a18150c949cb6
Madhan <madhanofficial08@gmail.com>
2026-01-25 16:03:07 +0530

    ci commit

--- src/main/java/utilities/random/Randomnumber.java
+++ src/main/java/utilities/random/Randomnumber.java
@@ -4,6 +4,7 @@ public class Randomnumber
 {
     public static String getEmployeeId()
     {
+        System.out.println();
         Random obj = new Random();
         return String.valueOf(obj.nextInt(90000)+10000);
     }
