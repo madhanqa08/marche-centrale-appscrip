1065efcd83ab29b984b380bd75c84c9cc696adae
Madhan <madhanofficial08@gmail.com>
2026-05-28 00:15:23 +0530

    Clean database config

--- src/test/java/mysqldatabase/DatabaseConnection.java
+++ src/test/java/mysqldatabase/DatabaseConnection.java
@@ -1,48 +1,54 @@
 package mysqldatabase;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class DatabaseConnection
 {
     private static Connection connection;
 
     private static final String URL =
-            "jdbc:mysql://mysql-1a051f38-madhanoffice08-be00.c.aivencloud.com:17907/APPSCRIP?ssl-mode=REQUIRED";
+            System.getenv("DB_URL");
 
     private static final String USERNAME =
-            "avnadmin";
+            System.getenv("DB_USERNAME");
 
     private static final String PASSWORD =
-            "AVNS_qysMEC_PBTP4XC_XeuR";
+            System.getenv("DB_PASSWORD");
 
     public static Connection createConnection()
     {
         try
         {
-            if (connection == null || connection.isClosed())
+            if(connection == null || connection.isClosed())
             {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
-                connection = DriverManager.getConnection(
-                        URL,
-                        USERNAME,
-                        PASSWORD
-                );
+                connection =
+                        DriverManager.getConnection(
+                                URL,
+                                USERNAME,
+                                PASSWORD
+                        );
 
                 System.out.println("Database Connected Successfully");
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
         return connection;
     }
 
+
     public static void closeConnection()
     {
         try
         {
-            if (connection != null && !connection.isClosed())
+            if(connection != null && !connection.isClosed())
             {
                 connection.close();
                 System.out.println("Database Connection Closed");
