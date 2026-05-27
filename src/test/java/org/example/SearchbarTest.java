131ab181fabc7f4577586f0ef5dad9b2c2cd8232
Madhan <madhanofficial08@gmail.com>
2026-05-27 13:16:50 +0530

    first commit

--- src/test/java/org/example/SearchbarTest.java
+++ src/test/java/org/example/SearchbarTest.java
@@ -5,8 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class SearchbarTest extends BaseTest
 {
-
-
     SignupPojo signup = new SignupPojo();
     @Test(description = "Verify Profile Icon Visibility on Landing Page")
     public void testcase001()
@@ -14,6 +12,8 @@ public class SearchbarTest extends BaseTest
         boolean flag = loginPage.isprofilepresent();
         Assert.assertTrue(flag);
     }
+
+
 
 
     @Test(description = "Verify Click on Profile Icon Opens Sign In Modal")
