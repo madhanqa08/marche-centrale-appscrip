0373cb83dc45613ae24cb53a2d498d122a5688dc
Madhan <madhanofficial08@gmail.com>
2026-05-27 23:55:25 +0530

    first commit

--- src/test/java/mysqldatabase/DatabaseConnection.java
+++ src/test/java/mysqldatabase/DatabaseConnection.java
@@ -2,8 +2,6 @@ package mysqldatabase;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-
 public class DatabaseConnection
 {
     private static Connection connection;
